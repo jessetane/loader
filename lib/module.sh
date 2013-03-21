a9cc9597e1124b727852a1db6eced03c05612100
Jesse Tane <jesse.tane@gmail.com>
2013-03-20 21:34:54 -0700

    improve errors and warnings

--- lib/module.sh
+++ lib/module.sh
@@ -10,12 +10,12 @@ module() {
   local version="$3"
   
   # sanity
-  [ -z "$MODULE_ROOT" ] && echo '$MODULE_ROOT is not defined' >&2 && return 1
+  [ -z "$MODULE_ROOT" ] && echo "MODULE_ROOT is not defined" >&2 && return 1
   [ -z "$cmd" ] && echo "please specify a command" >&2 && return 1
-  [ -z "$name" ] && echo "please specify a module" >&2 && return 1
   
   # vars
-  local build="$MODULE_ROOT"/src/"$name"/build
+  local src="$MODULE_ROOT"/src/"$name"
+  local build="$src"/build
   
   # proxy to command
   case "$cmd" in
@@ -27,11 +27,15 @@ module() {
 
 __module_load() {
   
+  # sanity
+  [ -z "$name" ] && echo "please specify a module to load" >&2 && return 1
+  [ ! -d "$src" ] && echo "$name: module is not installed" >&2 && return 1
+  
   # set version to default if necessary
   [ -z "$version" ] && version="default"
   
   # is the requested version installed?
-  [ ! -d "$build"/"$version" ] && echo "version $version is not installed" >&2 && return 1
+  [ ! -d "$build"/"$version" ] && echo "$version: version not installed" >&2 && return 1
   
   # unload any currently loaded versions first
   __module_unload
@@ -46,6 +50,12 @@ __module_load() {
 }
 
 __module_unload() {
+  
+  # sanity
+  [ -z "$name" ] && echo "please specify a module to unload" >&2 && return 1
+  
+  # is the requested module installed?
+  [ ! -d "$src" ] && echo "$name: module is not installed" >&2 && return 1
   
   # only modify PATH if it's defined
   if [ -n "$PATH" ]
