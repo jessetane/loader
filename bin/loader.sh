b9b0917f65bac941d807c9a3c76e0589f89962e7
Jesse Tane <jesse.tane@gmail.com>
2013-03-21 00:45:48 -0700

    update variable names

--- bin/loader.sh
+++ bin/loader.sh
@@ -5,19 +5,19 @@
 load() {
   
   # args
-  local name="$1"
+  local package="$1"
   local version="$2"
   
   # sanity pt. I
   [ -z "$PKGPATH" ] && echo "PKGPATH is not defined" >&2 && return 1
-  [ -z "$name" ] && echo "please specify a package to load" >&2 && return 1
+  [ -z "$package" ] && echo "please specify a package to load" >&2 && return 1
   
   # vars
-  local src="$PKGPATH"/src/"$name"
+  local src="$PKGPATH"/src/"$package"
   local build="$src"/build
   
   # sanity pt. II
-  [ ! -d "$src" ] && echo "$name: package is not installed" >&2 && return 1
+  [ ! -d "$src" ] && echo "$package: package is not installed" >&2 && return 1
   
   # set version to default if necessary
   [ -z "$version" ] && version="default"
@@ -26,7 +26,7 @@ load() {
   [ ! -d "$build"/"$version" ] && echo "$version: version not installed" >&2 && return 1
   
   # unload any currently loaded versions first
-  unload "$name"
+  unload "$package"
   
   # don't place a ":" at the end of PATH
   [ -n "$PATH" ] && PATH=":$PATH"
@@ -39,18 +39,18 @@ load() {
 unload() {
   
   # args
-  local name="$1"
+  local package="$1"
   
   # sanity
   [ -z "$PKGPATH" ] && echo "PKGPATH is not defined" >&2 && return 1
-  [ -z "$name" ] && echo "please specify a package to unload" >&2 && return 1
+  [ -z "$package" ] && echo "please specify a package to unload" >&2 && return 1
   
   # vars
-  local src="$PKGPATH"/src/"$name"
+  local src="$PKGPATH"/src/"$package"
   local build="$src"/build
   
   # is the requested package installed?
-  [ ! -d "$src" ] && echo "$name: package is not installed" >&2 && return 1
+  [ ! -d "$src" ] && echo "$package: package is not installed" >&2 && return 1
   
   # remove package/version/bin from PATH
   PATH="$(echo "$PATH" | sed "s|$build/[^/]*/bin:||g")"
